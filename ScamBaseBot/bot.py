93264e83be1395b1114ca65995b5923e2cb8f48a
wezzek198 <modbrawl4@gmail.com>
2026-01-18 13:28:31 +0700

    Update bot.py

--- ScamBaseBot/bot.py
+++ ScamBaseBot/bot.py
@@ -2277,5 +2277,6 @@ async def main():
         raise
 
 if __name__ == '__main__':
-    asyncio.run(main())
-
+    # Просто запускаем main без asyncio.run()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
